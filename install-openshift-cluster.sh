480b51ed550b99220f872afffcfdca19c87a1447
naimabdrahman <naim.abdrahman@gmail.com>
2018-01-03 15:48:49 +0800

    Update install-openshift-cluster.sh

--- install-openshift-cluster.sh
+++ install-openshift-cluster.sh
@@ -20,11 +20,16 @@ cat /etc/ansible/ansible.cfg.bckup | sed 's/#host_key_checking = False/host_key_
 
 
 
-#To run the ansible installation
+# On Master Node, login with a user created above and run Ansible Playbook for setting up OpenShift Cluster. 
+yum -y install atomic-openshift-utils 
+
+
+# To run the ansible installation
 echo -e "\nPls run command below from the ansible master\nansible-playbook /usr/share/ansible/openshift-ansible/playbooks/byo/config.yml\n\n" 
 
 
 
+# Verification after ansible installation completed
 echo -e "\nverification after ansible installation completed"
 # show state
 echo -e "\noc get nodes"
